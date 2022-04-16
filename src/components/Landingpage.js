9025af88818a28ed5eb736dce32a5c02a068a2cd
pranavthakur0-0 <pranavthakurdew@gmail.com>
2022-04-16 20:47:14 +0530

    your message

---
+++ src/components/Landingpage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './landingpage.css';
+
+function Landingpage()
+{
+
+    return (
+        <>
+      <>asdf
+      </>
+
+    
+      
+        </>
+    );
+}
+export default Landingpage;
